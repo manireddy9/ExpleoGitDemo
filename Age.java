a758481c8a28ec00786e9a3107fe853b9bb7b3fc
manireddy9 <146085695+manireddy9@users.noreply.github.com>
2023-11-01 13:21:50 +0530

    Add files via upload

---
+++ Age.java
@@ -0,0 +1,14 @@
+/**
+* displaying the age
+* @ Mani Gopi Reddy Challa(Expleo)
+* @ 09-sep-2023
+*/
+
+public class Age{
+	public static void main(String[] args){
+		byte age;
+		age=20;                         //declaring and assign the value to the variable
+		System.out.println(age);
+		
+        }
+}
